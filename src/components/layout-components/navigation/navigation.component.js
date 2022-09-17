69e858f975f179379844976955c47599bc2a8a7a
Mario Burgos <mario.burpi@gmail.com>
2022-09-17 21:46:06 +0200

    Navigation header ready --> inspirado en una plantilla de codepen: 'es muy burdo pero yo voy con ello'
    Next steps:  make the links work properly, change text and create more components

--- src/components/layout-components/navigation/navigation.component.js
+++ src/components/layout-components/navigation/navigation.component.js
@@ -1,6 +1,11 @@
 export class NavigationComponent {
-  /**metodo de clase que va a cargar mediante jQuery el html allá donde se le llame*/
+  /**metodo de clase que va a cargar mediante jQuery el html (y el css en el head) allá donde se le llame*/
   render(selector) {
+    // inyectar css
+    $("head").append(
+        '<link rel="stylesheet" href="src/components/layout-components/navigation/navigation.component.css" type="text/css" />'
+      );
+      //inyectar html
     $(function () {
       $(selector).load(
         "src/components/layout-components/navigation/navigation.component.html"
